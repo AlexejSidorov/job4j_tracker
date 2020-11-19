0a7c9c15fb308a40a9e3347f0559315d1cb4ad1a
Alex Sidorov <cobetckoeyxo@mail.ru>
2020-11-19 22:07:38 +1000

    1.1. Создание объекта [#401590]

---
+++ src/main/java/ru/job4j/oop/Dog.java
@@ -0,0 +1,10 @@
+package ru.job4j.oop;
+
+public class Dog {
+    // polkan, sharik, zhychka
+    public static void main(String[] args) {
+        Dog polkan = new Dog();
+        Dog sharik = new Dog();
+        Dog zhychka = new Dog();
+    }
+}
