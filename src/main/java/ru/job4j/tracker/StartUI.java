7f4d1e3caa746c7cd7ec7a938ad4df5e8c52c948
Sidorov Aleksey <cobetckoeyxo@mail.ru>
2020-12-23 02:35:00 +1000

    1. Обеспечить бесперебойную работу приложения Tracker [#401566]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -30,7 +30,7 @@ public class StartUI {
 
     public static void main(String[] args) {
         Output out = new ConsoleOutput();
-        Input input = new ConsoleInput();
+        Input input = new ValidateInput();
         Tracker tracker = new Tracker();
         UserAction[] actions = {
                 new CreateAction(out),
