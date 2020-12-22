ff1eb5978b86cf78db4475ec98b44593ac6e3726
Sidorov Aleksey <cobetckoeyxo@mail.ru>
2020-12-23 02:38:59 +1000

    1.1. Тесты на StartUI. [#401564]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -207,6 +207,29 @@ public class StartUITest {
                         "1. Exit" + System.lineSeparator()
         ));
     }
+
+    @Test
+    public void whenInvalidExit() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] { "9", "0" }
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {
+                new ExitAction()
+        };
+        new StartUI(out).init(in, tracker, actions);
+        assertThat(out.toString(), is(
+                String.format(
+                        "Menu.%n"
+                                + "0. Exit%n"
+                                + "Wrong number. Try again.%n"
+                                + "Menu.%n"
+                                + "0. Exit%n"
+                )
+        ));
+    }
+
 }
 
 
